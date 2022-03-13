f68bd7d091369d71bad7246da055da0bc0bec000
miyu-codes <96804318+miyu-codes@users.noreply.github.com>
2022-03-13 12:45:41 +0900

    Created search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
-    <div>
+    <div className="container">
       <h1>Dictionary</h1>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+        <small>Coded by Miyu Hayakawa</small>
+      </footer>
     </div>
   );
 }
